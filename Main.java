1088676e9d3c534df12c75025548d07072daed6a
yemrecakar <77546840+Yemrecakar@users.noreply.github.com>
2021-12-24 21:19:10 +0000

    arrays

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import java.util.Scanner;//diziye eleman ekleme ve eklem-nilen elemanları 
+ 
+public class Main {
+    public static void main(String args[]) {
+         int n;
+         Scanner nesne=new Scanner(System.in);
+        System.out.println(" boyut giriniz ");//boyutu aldik
+          n=nesne.nextInt();
+
+    int dizi[] = new int[n];
+    int toplam=0;
+
+
+
+    for(int i=0;i<n;i++)
+    {
+    System.out.println((i+1)+". elemani giriniz:");
+      dizi[i]=nesne.nextInt();
+      toplam+=dizi[i]  ;
+      }
+           System.out.println("toplam = "+toplam);
+ 
+    }
 }
